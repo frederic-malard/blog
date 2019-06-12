9a538dbcc29d15ef3e34cd56284a7586663d6310
travail <travail@macbook-pro-de-frederic.home>
2019-06-12 21:19:24 +0200

    création page inscription etc, pause dans la création d'utilisateur et l'authentification

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,24 +2,47 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegistrationType;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
 {
     /**
      * @Route("/login", name="account_login")
      */
-    public function login()
+    public function login(AuthenticationUtils $utils)
     {
-        return $this->render('account/login.html.twig');
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
+        return $this->render('account/login.html.twig', [
+            'hasError' => $error !== null,
+            'username' => $username
+        ]);
     }
 
     /**
-     * @Route("/logout" name="account_logout")
+     * @Route("/logout", name="account_logout")
      */
     public function logout()
     {
-        
+
+    }
+
+    /**
+     * @Route("/inscription", name="account_register")
+     */
+    public function register()
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        return $this->render('account/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class RegistrationType extends AbstractType
+{
+    private function getConfiguration($label, $placeholder, $options = [])
+    {
+        return array_merge([
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ], $options);
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('login', TextType::class, $this->getConfiguration("login", "votre login (permettra de vous authentifier, identifiant) ..."))
+            ->add('hash', PasswordType::class, $this->getConfiguration("mot de passe", "votre mot de passe..."))
+            ->add('picture', UrlType::class, $this->getConfiguration("avatar", "l'url (chemin) de votre avatar (image de profile)", ['required' => false]))
+            ->add('email', EmailType::class, $this->getConfiguration("email", "votre email"))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
